245c00df0b2e681fa7521233a7141fbf0e735351
Colm Bhandal <bhandalc@gmail.com>
2023-08-16 11:29:56 +0100

    work dir
    Passing working directory as an arg

--- inclusive-language/inclusive_language.py
+++ inclusive-language/inclusive_language.py
@@ -1,12 +1,15 @@
 import subprocess
+import sys
 
-def run_command(command):
-    subprocess.run(command, check=True, shell=True)
+def run_command(command, cwd):
+    subprocess.run(command, check=True, shell=True, cwd=cwd)
+
+working_directory = sys.argv[1]
 
 try:
     # Install the doc framework and run inclusive-language checker
-    run_command('make install')
-    run_command('make woke')
+    run_command('make install', working_directory)
+    run_command('make woke', working_directory)
 except subprocess.CalledProcessError as e:
     print(f"Command '{e.cmd}' returned non-zero exit status {e.returncode}.")
     exit(1)
--- spellcheck/spellcheck.py
+++ spellcheck/spellcheck.py
@@ -1,15 +1,18 @@
 import subprocess
+import sys
 
-def run_command(command):
-    subprocess.run(command, check=True, shell=True)
+def run_command(command, cwd):
+    subprocess.run(command, check=True, shell=True, cwd=cwd)
+
+working_directory = sys.argv[1]
 
 try:
     # Install Aspell
-    run_command('sudo apt-get install aspell aspell-en')
+    run_command('sudo apt-get install aspell aspell-en', working_directory)
 
     # Install the doc framework and run spelling checker
-    run_command('make install')
-    run_command('make spelling')
+    run_command('make install', working_directory)
+    run_command('make spelling', working_directory)
 except subprocess.CalledProcessError as e:
     print(f"Command '{e.cmd}' returned non-zero exit status {e.returncode}.")
     exit(1)
