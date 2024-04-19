44d743e04e145f85375c5aa1b12d281532d8406a
Artem Konev <artem.konev@canonical.com>
2024-04-19 16:07:13 +0100

    feat: fall back to starter pack Makefile

--- inclusive-language/inclusive_language.py
+++ inclusive-language/inclusive_language.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import subprocess
 
@@ -7,9 +8,14 @@ def run_command(command, cwd):
 working_directory = sys.argv[1]
 
 try:
+    # If the starter pack Makefile is available, use it
+    makefile = "Makefile"
+    if os.path.exists(os.path.join(working_directory, "Makefile.sp")):
+        makefile = "Makefile.sp"
+
     # Install the doc framework and run inclusive-language checker
-    run_command('make install', working_directory)
-    run_command('make woke', working_directory)
+    run_command(f"make -f {makefile} install", working_directory)
+    run_command(f"make -f {makefile} woke", working_directory)
 except subprocess.CalledProcessError as e:
     print(f"Command '{e.cmd}' returned non-zero exit status {e.returncode}.")
     exit(1)
--- spellcheck/spellcheck.py
+++ spellcheck/spellcheck.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import subprocess
 
@@ -10,9 +11,14 @@ try:
     # Install Aspell
     run_command('sudo apt-get install aspell aspell-en', working_directory)
 
+    # If the starter pack Makefile is available, use it
+    makefile = "Makefile"
+    if os.path.exists(os.path.join(working_directory, "Makefile.sp")):
+        makefile = "Makefile.sp"
+
     # Install the doc framework and run spelling checker
-    run_command('make install', working_directory)
-    run_command('make spelling', working_directory)
+    run_command(f"make -f {makefile} install", working_directory)
+    run_command(f"make -f {makefile} spelling", working_directory)
 except subprocess.CalledProcessError as e:
     print(f"Command '{e.cmd}' returned non-zero exit status {e.returncode}.")
     exit(1)
